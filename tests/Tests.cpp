270418fc837232b15fc9a80a6c521a73e309e231
Elisey <eliseyta2004@gmail.com>
2022-12-19 11:24:26 +0300

    Second debug attempt

--- tests/Tests.cpp
+++ tests/Tests.cpp
@@ -24,33 +24,33 @@ TEST_CASE("multiplication testing") {
     CHECK(multiplication(-3, -3) == 9);
 }
 
-//TEST_CASE("division testing") {
-//    CHECK(division(3, 3) == 1);
-//    CHECK(division(-3, 3) == -1);
-//    CHECK(division(3, -3) == -1);
-//    CHECK(division(-3, -3) == 1);
-//}
-//
-//TEST_CASE("percent testing") {
-//    CHECK(percent_of(1) == 0.01);
-//    CHECK(percent_of(50) == 0.5);
-//    CHECK(percent_of(75) == 0.75);
-//    CHECK(percent_of(100) == 1);
-//}
-//
-//TEST_CASE("pow testing") {
-//    CHECK(pow(1, 0) == 1);
-//    CHECK(pow(1, 3) == 1);
-//    CHECK(pow(2, 0) == 1);
-//    CHECK(pow(2, 2) == 4);
-//    CHECK(pow(2, 10) == 1024);
-//}
-//
-//TEST_CASE("factorial testing") {
-//    CHECK(factorial(1) == 1);
-//    CHECK(factorial(2) == 2);
-//    CHECK(factorial(3) == 6);
-//    CHECK(factorial(5) == 120);
-//    CHECK(factorial(8) == 40320);
-//}
+TEST_CASE("division testing") {
+    CHECK(division(3, 3) == 1);
+    CHECK(division(-3, 3) == -1);
+    CHECK(division(3, -3) == -1);
+    CHECK(division(-3, -3) == 1);
+}
+
+TEST_CASE("percent testing") {
+    CHECK(percent_of(1) == 0.01);
+    CHECK(percent_of(50) == 0.5);
+    CHECK(percent_of(75) == 0.75);
+    CHECK(percent_of(100) == 1);
+}
+
+TEST_CASE("pow testing") {
+    CHECK(pow(1, 0) == 1);
+    CHECK(pow(1, 3) == 1);
+    CHECK(pow(2, 0) == 1);
+    CHECK(pow(2, 2) == 4);
+    CHECK(pow(2, 10) == 1024);
+}
+
+TEST_CASE("factorial testing") {
+    CHECK(factorial(1) == 1);
+    CHECK(factorial(2) == 2);
+    CHECK(factorial(3) == 6);
+    CHECK(factorial(5) == 120);
+    CHECK(factorial(8) == 40320);
+}
 
