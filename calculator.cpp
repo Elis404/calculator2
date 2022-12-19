8b6eac2d2a7ff717982c87972eacdf1570defff7
Elisey <eliseyta2004@gmail.com>
2022-12-19 11:09:57 +0300

    Upload all

---
+++ calculator.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "calculator.h"
+
+double addition(double a, double b) {
+    return a + b;
+}
+
+double subtraction(double a, double b) {
+    return a - b;
+}
+
+double multiplication(double a, double b) {
+    return a * b;
+}
+
+double division(double a, double b) {
+    return a / b;
+}
+
+double percent_of(double a) {
+    return a / 100;
+}
+
+double pow(double a, double n) {
+    int res = 1;
+    for (int i = 0; i < n; i++) {
+        res *= a;
+    }
+    return res;
+}
+
+double factorial(double a) {
+    return a <= 1 ? a : factorial(a - 1) * a;
+}
+
---
+++ calculator.h
@@ -0,0 +1,19 @@
+#ifndef CALCULATOR2_CALCULATOR_H
+#define CALCULATOR2_CALCULATOR_H
+
+double addition(double a, double b);
+
+double subtraction(double a, double b);
+
+double multiplication(double a, double b);
+
+double division(double a, double b);
+
+double percent_of(double a);
+
+double pow(double a, double n);
+
+double factorial(double a);
+
+
+#endif //CALCULATOR2_CALCULATOR_H
